72c56cda73f2feadbb9529c75d6cf3b163072a40
Harwinder Singh <91020737+Harwinder33Singh@users.noreply.github.com>
2023-10-05 18:35:06 -0700

    Add files via upload

---
+++ CSE355 Lab-1/CSE355 Lab-1/CSE355_Lab_1App.swift
@@ -0,0 +1,17 @@
+//
+//  CSE355_Lab_1App.swift
+//  CSE355 Lab-1
+//
+//  Created by Harwinder Singh on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CSE355_Lab_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
